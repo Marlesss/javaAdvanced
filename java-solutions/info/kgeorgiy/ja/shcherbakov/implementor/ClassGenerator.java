5c3070f5537d5ed59e48bce442e23bf473c192b3
Marlesss <334874@niuitmo.ru>
2023-03-17 16:40:26 +0300

    HW4

---
+++ java-solutions/info/kgeorgiy/ja/shcherbakov/implementor/ClassGenerator.java
@@ -0,0 +1,173 @@
+package info.kgeorgiy.ja.shcherbakov.implementor;
+
+import info.kgeorgiy.java.advanced.implementor.ImplerException;
+
+import java.lang.reflect.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ClassGenerator {
+
+    private static final String LINE_SEP = System.lineSeparator();
+    private static final String OVERRIDE = "@Override";
+    private final Class<?> token;
+
+    ClassGenerator(Class<?> token) {
+        this.token = token;
+    }
+
+    public static String getClassName(Class<?> token) {
+        return token.getSimpleName() + "Impl";
+    }
+
+    public String generate() throws ImplerException {
+        StringBuilder sb = new StringBuilder();
+        generatePackage(sb);
+        generateHeader(sb);
+        generateConstructors(sb);
+        generateMethods(sb);
+        generateFooter(sb);
+        return sb.toString();
+    }
+
+    private static String getPadding(int size) {
+        return "\t".repeat(size);
+    }
+
+    private static String getParameters(Executable executable, boolean withTypes) {
+        StringJoiner stringJoiner = new StringJoiner(", ", "(", ")");
+        for (Parameter parameter : executable.getParameters()) {
+            stringJoiner.add((withTypes ? parameter.getType().getCanonicalName() + " " : "") + parameter.getName());
+        }
+        return stringJoiner.toString();
+    }
+
+    private static String generateThrows(Executable method) {
+        Class<?>[] exceptions = method.getExceptionTypes();
+        if (exceptions.length == 0) {
+            return "";
+        }
+
+        StringJoiner sj = new StringJoiner(", ", " throws ", "");
+        for (Class<?> exception : exceptions) {
+            sj.add(exception.getCanonicalName());
+        }
+        return sj.toString();
+    }
+
+    private static String generateReturn(Method method) {
+        if (method.getReturnType() == void.class) {
+            return "";
+        }
+        return "return " + getDefaultValue(method.getReturnType());
+    }
+
+    private static String generateSuper(Constructor<?> constructor) {
+        return "super" + getParameters(constructor, false);
+    }
+
+    private static String getDefaultValue(Class<?> returnType) {
+        if (returnType == boolean.class) {
+            return "false";
+        }
+        if (returnType.isPrimitive()) {
+            return "0";
+        }
+        return "null";
+    }
+
+    private void generatePackage(StringBuilder sb) {
+        sb.append("package ").append(token.getPackage().getName()).append(';').append(LINE_SEP).append(LINE_SEP);
+    }
+
+    private void generateHeader(StringBuilder sb) {
+        sb.append("public class ").append(getClassName(token)).append(' ');
+        if (token.isInterface()) {
+            sb.append("implements ");
+        } else {
+            sb.append("extends ");
+        }
+        sb.append(token.getCanonicalName()).append(" {").append(LINE_SEP);
+    }
+
+    private void generateConstructors(StringBuilder sb) throws ImplerException {
+        if (token.isInterface()) {
+            return;
+        }
+        List<Constructor<?>> constructors = Arrays.stream(token.getDeclaredConstructors())
+                .filter((constructor -> !Modifier.isPrivate(constructor.getModifiers()))).toList();
+        if (constructors.size() == 0) {
+            throw new ImplerException("There is no default constructor available");
+        }
+        constructors.forEach(constructor -> generateExecutable(sb, constructor));
+    }
+
+    private static class UniqueMethod {
+        public final Method method;
+
+        UniqueMethod(Method method) {
+            this.method = method;
+        }
+
+        @Override
+        public int hashCode() {
+            return (method.getName().hashCode() * 17 +
+                    method.getReturnType().hashCode()) * 17 +
+                    Arrays.hashCode(method.getParameterTypes());
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj == null) {
+                return false;
+            }
+
+            if (obj instanceof UniqueMethod uniqueMethod) {
+                return method.getName().equals(uniqueMethod.method.getName()) &&
+                        method.getReturnType() == uniqueMethod.method.getReturnType() &&
+                        Arrays.equals(method.getParameterTypes(), uniqueMethod.method.getParameterTypes());
+            }
+            return false;
+        }
+    }
+
+    private void generateMethods(StringBuilder sb) {
+        Set<UniqueMethod> methods = new HashSet<>();
+        Arrays.stream(token.getMethods()).map(UniqueMethod::new).collect(Collectors.toCollection(() -> methods));
+        Class<?> tempToken = token;
+        while (tempToken != null) {
+            Arrays.stream(tempToken.getDeclaredMethods()).map(UniqueMethod::new)
+                    .collect(Collectors.toCollection(() -> methods));
+            tempToken = tempToken.getSuperclass();
+        }
+        methods.stream().filter(method -> Modifier.isAbstract(method.method.getModifiers()))
+                .forEach(method -> generateExecutable(sb, method.method));
+    }
+
+    private void generateExecutable(StringBuilder sb, Executable executable) {
+        if (executable instanceof Method) {
+            sb.append(getPadding(1)).append(OVERRIDE).append(LINE_SEP);
+        }
+        sb.append(getPadding(1)).append(Modifier.isPublic(executable.getModifiers()) ? "public " : "protected ");
+        if (executable instanceof Method method) {
+            sb.append(method.getReturnType().getTypeName()).append(' ').append(method.getName());
+        } else {
+            sb.append(getClassName(token));
+        }
+        sb.append(getParameters(executable, true))
+                .append(generateThrows(executable)).append(" {").append(LINE_SEP);
+
+        sb.append(getPadding(2));
+        if (executable instanceof Method method) {
+            sb.append(generateReturn(method));
+        } else {
+            sb.append(generateSuper((Constructor<?>) executable));
+        }
+        sb.append(';').append(LINE_SEP)
+                .append(getPadding(1)).append('}').append(LINE_SEP);
+    }
+
+    private void generateFooter(StringBuilder sb) {
+        sb.append('}');
+    }
+}
---
+++ java-solutions/info/kgeorgiy/ja/shcherbakov/implementor/Implementor.java
@@ -0,0 +1,77 @@
+package info.kgeorgiy.ja.shcherbakov.implementor;
+
+import info.kgeorgiy.java.advanced.implementor.Impler;
+import info.kgeorgiy.java.advanced.implementor.ImplerException;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Modifier;
+import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Implementor implements Impler {
+
+    public static void main(String[] args) {
+        if (args == null || args.length != 2 || args[0] == null || args[1] == null) {
+            System.err.println("Wrong arguments");
+            System.err.println("java Implementor class_name root_path");
+            return;
+        }
+
+
+        try {
+            new Implementor().implement(Class.forName(args[0]), Paths.get(args[1]));
+        } catch (ClassNotFoundException e) {
+            System.err.println("Class " + args[0] + " not found: " + e.getMessage());
+        } catch (InvalidPathException e) {
+            System.err.println("Wrong path: " + e.getMessage());
+        } catch (ImplerException e) {
+            System.err.println("Error occurred while implementing:" + e.getMessage());
+        }
+    }
+
+    @Override
+    public void implement(Class<?> token, Path root) throws ImplerException {
+        int mods = token.getModifiers();
+        if (token.isPrimitive()) {
+            throw new ImplerException("Interface or Class token expected");
+        }
+
+        if (Modifier.isPrivate(mods)) {
+            throw new ImplerException("Can't implement private token");
+        }
+
+        if (!token.isInterface() && Modifier.isFinal(mods)) {
+            throw new ImplerException("Couldn't extend final class");
+        }
+
+        if (token == Enum.class) {
+            throw new ImplerException("Couldn't extend enum");
+        }
+
+        Path classPath = root.resolve(token.getPackageName().replace('.', File.separatorChar))
+                .resolve(ClassGenerator.getClassName(token) + ".java");
+        try {
+            Path classPathParent = classPath.getParent();
+            if (classPathParent != null) {
+                Files.createDirectories(classPathParent);
+            }
+        } catch (IOException e) {
+            System.err.println("Couldn't create directories on way to the generated class");
+            return;
+        }
+
+        try (
+                BufferedWriter writer = Files.newBufferedWriter(classPath)
+        ) {
+            ClassGenerator classGenerator = new ClassGenerator(token);
+            writer.write(classGenerator.generate());
+        } catch (IOException e) {
+            System.err.println("Error while writing generated class to path " + classPath);
+            System.err.println(e);
+        }
+    }
+}
