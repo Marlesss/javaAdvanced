4db0184a44ee72b21dbec97db39de72e1bef3f36
Marlesss <334874@niuitmo.ru>
2023-04-07 13:50:35 +0300

    HW7

--- java-solutions/module-info.java
+++ java-solutions/module-info.java
@@ -1,10 +1,14 @@
 module info.kgeorgiy.ja.shcherbakov {
     requires info.kgeorgiy.java.advanced.student;
     requires info.kgeorgiy.java.advanced.implementor;
+    requires info.kgeorgiy.java.advanced.concurrent;
 
     requires java.compiler;
     requires java.desktop;
 
     exports info.kgeorgiy.ja.shcherbakov.implementor;
     opens info.kgeorgiy.ja.shcherbakov.implementor;
+
+    exports info.kgeorgiy.ja.shcherbakov.concurrent;
+    opens info.kgeorgiy.ja.shcherbakov.concurrent;
 }
